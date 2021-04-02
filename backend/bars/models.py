3e605f93ed83d31b72a9c1428681056db3990564
viceentcb <viceentcb@gmail.com>
2021-04-02 12:41:15 +0200

    [Back]-[Model bars]

--- backend/bars/models.py
+++ backend/bars/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from core.models import TimestampedModel
+
+
+class Bar(TimestampedModel):
+    slug = models.SlugField(db_index=True, max_length=255, unique=True)
+    name = models.CharField(db_index=True, max_length=255)
+    description = models.TextField()
+    phone_number= models.CharField(max_length=14)
+    location= models.CharField(max_length=255)
+    valoration= models.DecimalField(max_digits=2, decimal_places=1)
+    image= models.ImageField(max_length=255)
+    
+    def __str__(self):
+        return self.name
