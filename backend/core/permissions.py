ccf79548873e65a23a3569094a627a431fd698ac
viceentcb <viceentcb@gmail.com>
2021-04-12 07:54:42 +0200

    [SeekBar Sprint 3]-[Back]-[Solved bug in permision -> IsOwner]

--- backend/core/permissions.py
+++ backend/core/permissions.py
@@ -15,13 +15,14 @@ class IsOwnerOrAdmin(permissions.BasePermission):
 
     def has_object_permission(self, request, view, slug=None):
 
+        if(request.user.is_staff or request.user.is_superuser):
+            return True
+
         try:
             bar_id =request.user.worker.referenceWorker.filter(slug=slug).values_list('id', flat=True)[0]
             check = Work.objects.filter(worker_id=request.user.worker.id, bar_id=bar_id, isBoss=True).values_list('worker_id', flat=True)[0]
             return True    
         except:
-            if(request.user.is_staff or request.user.is_superuser):
-                return True
             return False
         
         return True
