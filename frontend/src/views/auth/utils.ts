54b30da6d5f01ad15d18477e3e39385b99560c75
viceentcb <viceentcb@gmail.com>
2021-04-22 20:11:48 +0200

    [Finbow Sprint 4]-[Front]-[Errors Register]

--- frontend/src/views/auth/utils.ts
+++ frontend/src/views/auth/utils.ts
@@ -3,35 +3,21 @@ const CheckRegex = (regEx: any, text: string) => {
 };
 
 export const RegisterFormErrors = (values: any) => {
-  console.log(values.username);
-  console.log(values.email);
-  console.log(values.password);
-  console.log(values.repeatPassword);
-  console.log(values.username);
-  console.log(values.username);
-
-  const typesErrors: string[] = [];
+  const typesErrors: any[] = [];
 
   if (!CheckRegex(/^[a-zA-Z0-9]{3,16}$/, values.username)) {
-    console.log("USERNAME ERROR", values.username);
     typesErrors.push("username");
   }
 
-  if (
-    !CheckRegex(
-      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-      values.email
-    )
-  ) {
+  if (!CheckRegex(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,values.email)) {
     console.log("EMAIL ERROR", values.email);
     typesErrors.push("email");
   }
 
   if (!CheckRegex(/^[a-zA-Z0-9]{3,16}$/, values.password)) {
-    console.log("PASSWORD ERROR", values.password);
     typesErrors.push("password");
   } else if (values.password !== values.repeatPassword) {
-    typesErrors.push("repeat");
+    typesErrors.push({name: 'repeat', value:'La contraseña no es igual'});
   }
 
   return typesErrors ;
